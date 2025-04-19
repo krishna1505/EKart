30c07fbcfb7e6f6ec13fd41699009dd6f260167c
krishna@123 <ky2288922@gmail.com>
2025-04-19 19:35:00 +0530

    four commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import NavBar from './components/Navbar';
+import NavBar from './components/NavBar';
 import Sidebar from './components/Sidebar';
 import { Routes, Route } from 'react-router-dom';
 import Add from './pages/Add';
